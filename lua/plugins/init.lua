74917541e979483d37875c6b23d8ae07d2b24515
Julien Lenne <contact.lenne@gmail.com>
2025-11-14 08:07:49 +0800

    Revert "feat(mason): ensure goimports is installed"
    This reverts commit e9a0026968d1a31c9f2f2b5b02ebc9fab49ccaae.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -66,14 +66,6 @@ return {
     end,
   },
   {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "goimports",
-      },
-    },
-  },
-  {
     "MeanderingProgrammer/render-markdown.nvim",
     lazy = require("configs.render-markdown").lazy,
     cmd = require("configs.render-markdown").cmd,
