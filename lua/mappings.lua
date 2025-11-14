7f598db63360054a377ae3f7e5773beee2ee7a07
Julien Lenne <contact.lenne@gmail.com>
2025-11-14 08:01:13 +0800

    feat: add keymap to organize imports

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -32,3 +32,21 @@ map(
   ":lua require('base46').toggle_transparency()<CR>",
   { noremap = true, silent = true, desc = "Toggle Background Transparency" }
 )
+
+-- Organize imports
+local function organize_imports()
+  local params = vim.lsp.util.make_range_params()
+  params.context = { only = { "source.organizeImports" } }
+  local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+  for _, res in pairs(result or {}) do
+    for _, r in pairs(res.result or {}) do
+      if r.edit then
+        vim.lsp.util.apply_workspace_edit(r.edit)
+      else
+        vim.lsp.buf.execute_command(r)
+      end
+    end
+  end
+end
+
+map("n", "<leader>oi", organize_imports, { desc = "LSP: Organize Imports" })
